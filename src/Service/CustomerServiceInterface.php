82f05137424429b6131ef84a8373fe8fc69ae5f4
Valentin MOREAU <vmoreau@datasolution.fr>
2023-02-28 00:45:16 +0100

    add route delete (customerController)

--- src/Service/CustomerServiceInterface.php
+++ src/Service/CustomerServiceInterface.php
@@ -7,4 +7,5 @@ use App\Entity\Customer;
 interface CustomerServiceInterface
 {
   public function create(Customer $customer, int $vendorId): ?Customer;
+  public function delete(Customer $customer); // @TODO voir ce que je mets en return value
 }
