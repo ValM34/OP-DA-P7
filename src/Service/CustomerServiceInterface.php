db738c92904ebe971d57e9c53df9242246ae2a42
Valentin MOREAU <vmoreau@datasolution.fr>
2023-03-07 15:58:26 +0100

    correct route app_customer_delete

--- src/Service/CustomerServiceInterface.php
+++ src/Service/CustomerServiceInterface.php
@@ -4,9 +4,10 @@ namespace App\Service;
 
 use App\Entity\Customer;
 use App\Entity\Vendor;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 interface CustomerServiceInterface
 {
   public function create(Customer $customer, Vendor $vendor): ?Customer;
-  public function delete(Customer $customer); // @TODO voir ce que je mets en return value
+  public function delete(Vendor $vendor, Customer $customer): JsonResponse;
 }
