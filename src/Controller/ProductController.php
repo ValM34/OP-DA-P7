fda2992c47b82f3eb9c4c11efc3d657dbbd3f3b8
Valentin MOREAU <vmoreau@datasolution.fr>
2023-03-14 15:13:17 +0100

    delete controller /api/test and change annotations to attributes

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,7 @@ use App\Entity\Product;
 use App\Repository\ProductRepository;
 use JMS\Serializer\SerializerInterface;
 use Nelmio\ApiDocBundle\Annotation\Model;
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 use App\Service\ProductServiceInterface;
 use Knp\Component\Pager\PaginatorInterface;
 
@@ -25,37 +25,26 @@ class ProductController extends AbstractController
   {}
 
   /**
-  * Cette méthode permet de récupérer l'ensemble des produits.
-  *
-  * @OA\Response(
-  *     response=200,
-  *     description="Retourne la liste des produits",
-  *     @OA\JsonContent(
-  *        type="array",
-  *        @OA\Items(ref=@Model(type=Product::class, groups={"products"}))
-  *     )
-  * )
-  * @OA\Parameter(
-  *     name="page",
-  *     in="query",
-  *     description="La page que l'on veut récupérer",
-  *     @OA\Schema(type="int")
-  * )
-  *
-  * @OA\Parameter(
-  *     name="limit",
-  *     in="query",
-  *     description="Le nombre d'éléments que l'on veut récupérer",
-  *     @OA\Schema(type="int")
-  * )
-  * @OA\Tag(name="Product")
-  *
-  * @param ProductRepository $productRepository
-  * @param SerializerInterface $serializer
-  * @param Request $request
-  * @return JsonResponse
-  */
-  // GET ALL
+   * GET ALL
+   */
+  #[OA\Response(
+    response: 200,
+    description: "Retourne la liste des produits",
+    content: new OA\JsonContent(ref: new Model(type: Product::class, groups: ['products']))
+  )]
+  #[OA\Parameter(
+    name: 'page',
+    in: 'query',
+    description: "La page que l'on veut récupérer",
+    schema: new OA\Schema(type: 'int')
+  )]
+  #[OA\Parameter(
+    name: 'limit',
+    in: 'query',
+    description: "Le nombre d'éléments que l'on veut récupérer",
+    schema: new OA\Schema(type: 'int')
+  )]
+  #[OA\Tag(name: 'Product')]
   #[Route('/api/product/all', name: 'app_product_get_all', methods: 'GET')]
   public function getAll(Request $request): JsonResponse
   {
@@ -65,46 +54,19 @@ class ProductController extends AbstractController
   }
 
   /**
-  * Cette méthode permet de récupérer un produit en renseignant son id.
-  *
-  * @OA\Response(
-  *     response=200,
-  *     description="Retourne un produit dont l'id en renseigné en paramètre de l'url.",
-  *     @OA\JsonContent(
-  *        type="array",
-  *        @OA\Items(ref=@Model(type=Product::class, groups={"product"}))
-  *     )
-  * )
-  *
-  * @OA\Tag(name="Product")
-  *
-  * @param ProductRepository $productRepository
-  * @param SerializerInterface $serializer
-  * @param Request $request
-  * @return JsonResponse
-  */
-  // GET ONE
+   * GET ONE
+   */
+  #[OA\Response(
+    response: 200, 
+    description: "Retourne un produit dont l'id est renseigné en paramètre de l'url.",
+    content: new OA\JsonContent(ref: new Model(type: Product::class, groups: ['product']))
+  )]
+  #[OA\Tag(name: 'Product')]
   #[Route('/api/product/{id}', name: 'app_product_get_one', methods: 'GET')]
   public function getOne(Product $product): JsonResponse
   {
     $jsonProduct = $this->productService->getOne($product);
 
     return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
-  }
-
-  #[Route('/api/test', name: 'app_test', methods: 'GET')]
-  public function test(Product $product, PaginatorInterface $paginator, Request $request)
-  {
-    $productList = $this->productRepository->findAll();
-
-    $pagination = $paginator->paginate(
-      $productList,
-      $request->query->getInt('page', 1),
-      $request->query->getInt('limit', 10)
-    );
-
-    $jsonProductList = $this->serializer->serialize($pagination, 'json');
-
-    return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
   }
 }
